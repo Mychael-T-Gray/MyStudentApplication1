006400ef1aa9916d355d3f202b39d63c31f65654
Mychael Gray <mtoddgray13@gmail.com>
2023-03-22 10:42:03 +0400

    hello

--- app/src/main/java/com/example/myapplication/UI/CourseNotesAdapter.java
+++ app/src/main/java/com/example/myapplication/UI/CourseNotesAdapter.java
@@ -22,53 +22,7 @@ import com.example.myapplication.entities.CourseNotes;
 
 import java.util.List;
 
-/*public class CourseNotesAdapter extends RecyclerView.Adapter<CourseNotesAdapter.CourseNoteViewHolder> {
-    private List<CourseNotes> mCourseNotes;
-    private Context mContext;
 
-    public CourseNotesAdapter(Context context) {
-        mContext = context;
-    }
-
-    @NonNull
-    @Override
-    public CourseNoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.course_note_item, parent, false);
-        return new CourseNoteViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull CourseNoteViewHolder holder, int position) {
-        if (mCourseNotes != null) {
-            CourseNotes currentNote = mCourseNotes.get(position);
-            holder.courseNoteTextView.setText(currentNote.getNoteText());
-        } else {
-            holder.courseNoteTextView.setText("No notes available");
-        }
-    }
-
-    public void setCourseNotes(List<CourseNotes> courseNotes) {
-        mCourseNotes = courseNotes;
-        notifyDataSetChanged();
-    }
-
-    @Override
-    public int getItemCount() {
-        if (mCourseNotes != null)
-            return mCourseNotes.size();
-        else return 0;
-    }
-
-    public class CourseNoteViewHolder extends RecyclerView.ViewHolder {
-        private final TextView courseNoteTextView;
-
-        private CourseNoteViewHolder(View itemView) {
-            super(itemView);
-            courseNoteTextView = itemView.findViewById(R.id.courseNoteItem);
-        }
-    }
-}*/
 public class CourseNotesAdapter extends RecyclerView.Adapter<CourseNotesAdapter.CourseNoteViewHolder> {
     private List<CourseNotes> mCourseNotes;
     private Context mContext;
@@ -134,6 +88,7 @@ public class CourseNotesAdapter extends RecyclerView.Adapter<CourseNotesAdapter.
         final EditText input = new EditText(context);
         input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
         builder.setView(input);
+        builder.setMessage(note.getNoteText());
 
         // Set up the buttons
         builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
