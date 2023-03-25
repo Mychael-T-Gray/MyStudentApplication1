b4fed0abaca38c8d896266ae39c38772383fbb41
Mychael Gray <mtoddgray13@gmail.com>
2023-03-25 09:35:59 +0400

    hello

--- app/src/main/java/com/example/myapplication/UI/TermList.java
+++ app/src/main/java/com/example/myapplication/UI/TermList.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.UI;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,6 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
 
 import com.example.myapplication.Database.Repository;
 import com.example.myapplication.R;
@@ -18,10 +24,16 @@ import java.util.List;
 public class TermList extends AppCompatActivity {
 private Repository repository;
 private Terms term;
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+
+
+
+        setTitle("Add Term");
         RecyclerView termListLRecyclerView = findViewById(R.id.termListRecyclerView);
         final TermAdapter termAdapter = new TermAdapter(this);
         termListLRecyclerView.setAdapter(termAdapter);
@@ -32,21 +44,33 @@ private Terms term;
         repository.getmAllTerms().observe(this, new Observer<List<Terms>>() {
             @Override
             public void onChanged(List<Terms> terms) {
-                // Update the RecyclerView's data when the terms list changes
+
                 termAdapter.setTerms(terms);
             }
         });
 
-
-
-        FloatingActionButton fab = findViewById(R.id.termListFloatingActionButton);
-        fab.setOnClickListener(view -> {
-
-            Intent intent = new Intent(TermList.this, TermDetails.class);
-            startActivity(intent);
-    });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_term:
+                Intent intent = new Intent(TermList.this, TermDetails.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_term_list, menu);
+        return true;
+    }
     @Override
     protected void onResume(){
 
